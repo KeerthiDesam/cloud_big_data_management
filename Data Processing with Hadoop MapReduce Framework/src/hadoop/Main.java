8510bd6d7d94b0afea13ef8192375a42185111cd
KeerthiDesam <keerthidesam@gmail.com>
2025-07-23 12:57:01 -0500

    ADD Data Processing with Hadoop MapReduce Framework files

---
+++ Data Processing with Hadoop MapReduce Framework/src/hadoop/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hadoop;
+
+import java.io.IOException;
+
+/**
+ * @author Chandrashekar Akkenapally
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String inputFileName = args[0];
+	}
+
+}
